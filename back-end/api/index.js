9767216a4849d1d2f5cef52f198e83f174cd9894
Douglas Eduardo <douglaseduardo220483@gmail.com>
2020-06-17 18:11:55 -0300

    terminado a criação do usuário

--- back-end/api/index.js
+++ back-end/api/index.js
@@ -2,6 +2,6 @@ const app = require('./server');
 
 const PORT = process.env.PORT || 3001;
 
-app.factory().listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Conectado na porta http://localhost:${PORT}`);
 });
