b131549cda7fca5dd0d65b451fe1d28ac130fbf4
antoniopaolillo <antonio_francisco_p@live.com>
2020-06-17 17:27:26 -0300

    codeCLimate

--- front-end/src/service.js
+++ front-end/src/service.js
@@ -2,7 +2,7 @@ export function validateLogin(setIsAdmin, setIsLoged) {
   const user = localStorage.getItem('user');
   if (!user) return;
   async function getUserData() {
-    const result = await fetch('http://localhost/3001/login', { method: 'GET', headers: user }).then(res => res.json());
+    const result = await fetch('http://localhost:3001/login', { method: 'GET', headers: user }).then(res => res.json());
     if (!result) return false;
     if (result.role) setIsAdmin(true);
     setIsLoged(true);
@@ -12,7 +12,7 @@ export function validateLogin(setIsAdmin, setIsLoged) {
 
 export async function sendData(event, data, setShouldRedirect, url) {
   event.preventDefault();
-  const result = await fetch(`http://localhost/3001/${url}`, { method: 'POST', headers: data }).then(res => res.json());
+  const result = await fetch(`http://localhost:3001/${url}`, { method: 'POST', headers: data }).then(res => res.json());
   localStorage.setItem('user', JSON.stringify(result));
   setShouldRedirect(true);
 }
