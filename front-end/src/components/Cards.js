66a28e6ad4b746907f1efc3d83883070963b1919
Douglas Eduardo <douglaseduardo220483@gmail.com>
2020-06-19 17:44:12 -0300

    mais um card. agora tirei o limite do grid

--- front-end/src/components/Cards.js
+++ front-end/src/components/Cards.js
@@ -1,20 +1,15 @@
 import React from 'react';
-// import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
-// import CameraIcon from '@material-ui/icons/PhotoCamera';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-// import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
-// import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
-// import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles(theme => ({
   icon: {
@@ -56,34 +51,30 @@ function Cards(props) {
   return (
     <div>
       <Container className={classes.cardGrid} maxWidth='md'>
-        {/* End hero unit */}
-        {/* <Grid container spacing={4}> */}
-          {/* {images.map(card => ( */}
-          <Grid item key={id} xs={12} sm={6} md={4}>
-            <Card className={classes.card}>
-              <CardMedia
-                className={classes.cardMedia}
-                image={image}
-                title={description}
-              />
-              <CardContent className={classes.cardContent}>
-                <Typography gutterBottom variant='h5' component='h2'>
-                  {price}
-                </Typography>
-                <Typography>{description}</Typography>
-              </CardContent>
-              <CardActions>
-                <Button size='small' color='primary'>
-                  <RemoveCircleIcon />
-                </Button>
-                <span>{quantity}</span>
-                <Button size='small' color='primary'>
-                  <AddCircleIcon />
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-        {/* </Grid> */}
+        <Grid item key={id} xs={12} sm={6} md={4}>
+          <Card className={classes.card}>
+            <CardMedia
+              className={classes.cardMedia}
+              image={image}
+              title={description}
+            />
+            <CardContent className={classes.cardContent}>
+              <Typography gutterBottom variant='h5' component='h2'>
+                {price}
+              </Typography>
+              <Typography>{description}</Typography>
+            </CardContent>
+            <CardActions>
+              <Button size='small' color='primary'>
+                <RemoveCircleIcon />
+              </Button>
+              <span>{quantity}</span>
+              <Button size='small' color='primary'>
+                <AddCircleIcon />
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
       </Container>
     </div>
   );
