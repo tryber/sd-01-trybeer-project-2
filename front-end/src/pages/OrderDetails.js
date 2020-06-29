5b80f93b54be9d54ce0bc3f5604eb7711600579d
Guilherme Luiz <guiiluiz44@gmail.com>
2020-06-29 18:16:02 -0300

    client order details cant mark as delivered

--- front-end/src/pages/OrderDetails.js
+++ front-end/src/pages/OrderDetails.js
@@ -45,7 +45,7 @@ function renderDetails(params) {
         {renderProducts(products, classes)}
         <h1 className={classes.totalPrice}>Total: <span data-testid="order-total-value">R$ {price}</span></h1>
       </div>
-      {!finished && <button data-testid="mark-as-delivered-btn" onClick={() => updateStatus(user, id, setShowButton)}>Marcar como Entregue</button>}
+      {(user.role && !finished) && <button data-testid="mark-as-delivered-btn" onClick={() => updateStatus(user, id, setShowButton)}>Marcar como Entregue</button>}
     </div>
   );
 }
